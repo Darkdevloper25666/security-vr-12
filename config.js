96e8684c5ed615763691356b82fb012a262c1268
Darkdevloper25666 <93795942+Darkdevloper25666@users.noreply.github.com>
2021-12-09 15:01:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     defaultPrefix: 'h!',
-    ownerID: '681553671364018196',
+    ownerID: '764498341312331827',
     _limits: 'The following are defaults.',
     adminCanChangeLimits: true,
     limits: {
